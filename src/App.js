c7db598134cb8be0a6deac833ace070df3b6edd7
React App <yusufsaifymurtaza@gmail.com>
2023-08-09 12:13:00 +0530

    changing title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import Form from './Components/Form';
 import Navbar from './Components/Navbar';
-import About from './Components/About';
 import { useState } from 'react';
 import Alert from './Components/Alert';
 
@@ -30,18 +29,26 @@ function App() {
       document.body.style.backgroundColor='#067a97'
       document.body.style.color='white'
       showAlert('Dark Mode Enabled','success')
+      setInterval(() => {
+        document.title='Textutils-is amazing app'
+      }, 2000);
+      setInterval(() => {
+        document.title='Textutils app'
+      }, 1500);
      }else
      {
       setmode('light')
       document.body.style.backgroundColor='white'
       document.body.style.color='black'
       showAlert('Light Mode Enabled','success')
+      document.title='Textutils'
      }
      
   }  
+  
   return (
     <> 
-      <Navbar title="Hakimi Dry Fruits" mode={mode} toggleMode={toggle}  />
+      <Navbar title="Hakimi Dry Fruits" mode={mode} toggleMode={toggle}   />
       <Alert  alert={alert}/>
       <div className="container my-3" >
         <Form heading="Enter Text Here"  mode={mode} showAlert={showAlert}/>
